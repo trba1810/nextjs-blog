199cfddd2f977760ab5fb028c00122c1ff861711
trba1810 <trba8l@live.com>
2023-09-29 03:15:05 +0200

    create input component

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+"use client";
+
+import Input from "@/components/input/Input";
+import React, { FormEvent } from "react";
+import { useState } from "react";
+
+interface InitialStateProps {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const initialState: InitialStateProps = {
+  name: "",
+  email: "",
+  password: "",
+};
 
 const page = () => {
-  return <div>page</div>;
+  const [state, setState] = useState(initialState);
+
+  function handleChange(e: any) {
+    setState({ ...state, [e.target.name]: e.target.value });
+  }
+
+  const onSubmit = (event: FormEvent) => {};
+
+  return (
+    <form className="text-center" onSubmit={onSubmit}>
+      <div className="flex flex-col justify-center h-[450px] w-[350px] mx-auto gap-2">
+        <Input
+          placeholder="Name"
+          name="name"
+          id="name"
+          type="text"
+          onChange={handleChange}
+          value={state.name}
+        />
+        <Input
+          placeholder="Email"
+          name="email"
+          id="email"
+          type="email"
+          onChange={handleChange}
+          value={state.email}
+        />
+        <Input
+          placeholder="Password"
+          name="password"
+          id="password"
+          type="password"
+          onChange={handleChange}
+          value={state.password}
+        />
+        <button type="submit"></button>
+      </div>
+    </form>
+  );
 };
 
 export default page;
