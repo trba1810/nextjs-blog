74cb957523a3a816a333e57cae866f24fc87a120
trba1810 <trba8l@live.com>
2023-10-03 00:19:29 +0200

    finish navbar

---
+++ components/navbar/Navbar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { SafeUser } from "@/types";
+import Link from "next/link";
+import { signOut } from "next-auth/react";
+
+interface UserMenuProps {
+  currentUser: SafeUser | null;
+}
+
+export default function Navbar({ currentUser }: UserMenuProps) {
+  return (
+    <header>
+      <nav className="bg-gray-200 flex justify-between px-4 py-6 shadow-xl">
+        <div>{currentUser?.name}</div>
+        <div className="flex gap-4">
+          <Link href="/">Home</Link>
+          <Link href="/create">Create</Link>
+          {currentUser ? (
+            <button onClick={() => signOut()}>Sign out</button>
+          ) : (
+            <Link href={"/register"}></Link>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+}
