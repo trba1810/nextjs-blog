8db1aac3e0830ced9dc58d1efcb9d3da4bd06f2d
trba1810 <trba8l@live.com>
2023-10-05 22:57:23 +0200

    create post blog endpoint

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -1,6 +1,6 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
-import prisma from "../../lib/prismadb";
+import prisma from "../lib/prismadb";
 
 export async function getSession() {
   return await getServerSession(authOptions);
